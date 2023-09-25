6c468bfddbbb9374fb4ab6620a8c2447e26c0f19
Kooreis <kooreis.official@gmail.com>
2023-09-25 10:35:49 -0500

    In this update, the function 'longestIncreasingSubsequence' is defined. This function takes a vector of integers as input and returns the length of the longest increasing subsequence. It uses dynamic programming to solve the problem.

--- solution.cpp
+++ solution.cpp
@@ -1,3 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+int longestIncreasingSubsequence(std::vector<int> arr) {
+    if (arr.empty()) {
+        return 0;
+    }
+
+    std::vector<int> dp(arr.size(), 1);
+    for (int i = 1; i < arr.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j]) {
+                dp[i] = std::max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return *std::max_element(dp.begin(), dp.end());
+}
