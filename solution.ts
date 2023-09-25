a98643e437623375cc3d783bd262b76603d349d7
Kooreis <kooreis.official@gmail.com>
2023-09-25 10:35:49 -0500

    This update includes the creation of the result array. It iterates over the tailIndexes array in reverse order, starting from the last index, and fills the result array with the corresponding elements from the input array.

--- solution.ts
+++ solution.ts
@@ -1,26 +1,8 @@
-class LongestIncreasingSubsequence {
-    private static longestIncreasingSubsequence(arr: number[]): number[] {
-        let n = arr.length;
-        let tailIndexes = new Array(n).fill(0);
-        let prevIndices = new Array(n).fill(-1);
-        let length = 1;
-
-        for (let i = 1; i < n; i++) {
-            if (arr[i] < arr[tailIndexes[0]]) {
-                tailIndexes[0] = i;
-            } else if (arr[i] > arr[tailIndexes[length - 1]]) {
-                prevIndices[i] = tailIndexes[length - 1];
-                tailIndexes[length++] = i;
-            } else {
-                let l = -1;
-                let r = length - 1;
-                while (r - l > 1) {
-                    let m = l + (r - l) / 2;
-                    if (arr[tailIndexes[m]] >= arr[i]) r = m;
-                    else l = m;
-                }
-                let pos = r;
-                prevIndices[i] = tailIndexes[pos - 1];
-                tailIndexes[pos] = i;
-            }
+let i = tailIndexes[length - 1];
+        let res = new Array(length);
+        for (let j = length - 1; j >= 0; j--) {
+            res[j] = arr[i];
+            i = prevIndices[i];
         }
+
+        return res;
